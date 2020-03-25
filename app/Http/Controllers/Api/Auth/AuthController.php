b97a144f9683091a93da7b5659b4f7ccbca02051
ademirrocha <tiademir.rocha93@gmail.com>
2020-03-25 16:39:49 -0300

    add return user in login

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
             
             return response()->json([
     			'token' => $token->accessToken,
+                $user
     		]);
     	}
     }
